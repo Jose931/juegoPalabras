c88f1a1ede879e3cbe908ad9fd11dba4e4f0b5f2
jose <jlaresti93@gmail.com>
2022-11-13 20:29:09 +0100

    mostrando status cort

--- login.php
+++ login.php
@@ -1,30 +1,30 @@
 <?php
  
-// include('config.php');
-// session_start();
+include('config.php');
+session_start();
  
-// if (isset($_POST['login'])) {
+if (isset($_POST['login'])) {
  
-//     $username = $_POST['username'];
-//     $password = $_POST['password'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
  
-//     $query = $connection->prepare("SELECT * FROM users WHERE USERNAME=:username");
-//     $query->bindParam("username", $username, PDO::PARAM_STR);
-//     $query->execute();
+    $query = $connection->prepare("SELECT * FROM users WHERE USERNAME=:username");
+    $query->bindParam("username", $username, PDO::PARAM_STR);
+    $query->execute();
  
-//     $result = $query->fetch(PDO::FETCH_ASSOC);
+    $result = $query->fetch(PDO::FETCH_ASSOC);
  
-//     if (!$result) {
-//         echo '<p class="error">Username password combination is wrong!</p>';
-//     } else {
-//         if (password_verify($password, $result['PASSWORD'])) {
-//             $_SESSION['user_id'] = $result['ID'];
-//             echo '<p class="success">Congratulations, you are logged in!</p>';
-//         } else {
-//             echo '<p class="error">Username password combination is wrong!</p>';
-//         }
-//     }
-// }
+    if (!$result) {
+        echo '<p class="error">Username password combination is wrong!</p>';
+    } else {
+        if (password_verify($password, $result['PASSWORD'])) {
+            $_SESSION['user_id'] = $result['ID'];
+            echo '<p class="success">Congratulations, you are logged in!</p>';
+        } else {
+            echo '<p class="error">Username password combination is wrong!</p>';
+        }
+    }
+}
  
 ?>
 
